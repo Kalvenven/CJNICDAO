375935fc2d8d82cb47252f24af42c7799fa9f38c
Kalvenven <41784087+Kalvenven@users.noreply.github.com>
2018-08-09 18:37:30 +0800

    Setting up

---
+++ Cjnicdao.Calvin.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cjnicdao.Calvin.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
